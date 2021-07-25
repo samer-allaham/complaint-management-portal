dde282f14866d63dd898ab9ccff8f1e1cc9ad034
samer-allaham <samerallaham@yahoo.com>
2021-07-26 01:26:29 +0300

    added a new superuser

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,6 +2,37 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.base_user import BaseUserManager
+
+# to be able to create super users with using an email
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Users require an email field')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
 # Create your models here.
 class User(AbstractUser):
     name=models.CharField(max_length=255)
@@ -20,6 +51,8 @@ class User(AbstractUser):
        choices=STATUS,
        default=CUSTOMER,
   )
+
+    objects = UserManager()
 
     username=None
     USERNAME_FIELD='email'
